d7eafafdf2c770876f6aa58cd22709d87f6a896a
sumangitron <127018473+sumangitron@users.noreply.github.com>
2024-03-24 11:27:48 +0000

    solve

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increse = () => {
+    setCount(count + 1);
+  }
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+        <button onClick={increse}>Click me</button>
     </div>
   )
 }
